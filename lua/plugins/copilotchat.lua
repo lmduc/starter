56e1e9fe743d393b502ae29819030be3f033aa51
Duc Le <leminhducktvn@gmail.com>
2025-06-16 22:15:07 +0200

    fix: typo in rename prompt

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -1,13 +1,14 @@
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
   dependencies = {
+    { "zbirenbaum/copilot.lua" },
     { "nvim-lua/plenary.nvim", branch = "master" },
   },
   build = "make tiktoken",
   opts = {
     prompts = {
       Rename = {
-        prompt = "Please rename the variable correcttly in given selection based on context",
+        prompt = "Please rename the variable correctly in given selection based on context",
         selection = function(source)
           local select = require("CopilotChat.select")
           return select.visual(source)
