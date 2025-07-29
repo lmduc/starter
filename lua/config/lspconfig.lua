f15eb0e9e3e535ab5c5e069eb8d6a451197853a0
Duc Le <duc.le@soundcloud.com>
2025-07-29 18:04:17 +0200

    update

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,7 +1,14 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    require("lspconfig").gopls.setup({})
+    require("lspconfig").gopls.setup({
+      settings = {
+        gopls = {
+          usePlaceholders = true,
+          completeUnimported = true,
+        },
+      },
+    })
     require("lspconfig").metals.setup({})
   end,
 }
